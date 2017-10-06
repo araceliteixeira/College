e3c5909f7944838daa3a8be77f130a363606a8a7
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-10-06 07:22:05 -0400

    Initial Commit

---
+++ College/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  College
+//
+//  Created by MacStudent on 2017-10-06.
+//  Copyright © 2017 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
